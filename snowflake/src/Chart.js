dd5f3484bfab2023b1cf41b64189015cb32b0233
anback <anders.back@me.com>
2018-09-17 08:39:47 +0200

    löädf

--- snowflake/src/Chart.js
+++ snowflake/src/Chart.js
@@ -98,54 +98,84 @@ class CandleStickChartWithZoomPan extends React.Component {
 				xExtents={xExtents}
 				xAccessor={xAccessor}
 				displayXAccessor={displayXAccessor}>
-
-				<Chart id={1}
-					yExtents={d => [d.high, d.low]}
-					height={400}>
-					<YAxis axisAt="right" orient="right" ticks={5} zoomEnabled={zoomEvent} {...yGrid} />
-
-					<MouseCoordinateY
-						at="right"
-						orient="right"
-						displayFormat={format(".2f")} />
-
-					<CandlestickSeries />
-					<OHLCTooltip origin={[-40, 0]}/>
-					<ZoomButtons
-						onReset={this.handleReset}
-					/>
-				</Chart>
-				<Chart id={2} yExtents={d => d.volume} height={150} origin={(w, h) => [0, h - 700]}>
-					<YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".2s")} zoomEnabled={zoomEvent} />
-
-					<MouseCoordinateY at="left" orient="left" displayFormat={format(".4s")} />
-
-					<BarSeries yAccessor={d => d.volume} fill={(d) => d.close > d.open ? "#6BA583" : "#FF0000"} />
-				</Chart>
-				<Chart id={3} yExtents={(d: DataRow) => Math.sign(Math.random() - 0.5) * 200} height={150} origin={(w, h) => [0, h - 550]}>
-					<YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".2s")} zoomEnabled={zoomEvent} />
-
-					<MouseCoordinateX at="bottom" orient="bottom" displayFormat={timeFormat("%Y-%m-%d %H:%M:%S")} />
-					<MouseCoordinateY at="left" orient="left" displayFormat={format(".4s")} />
-
-					<BarSeries
-						yAccessor={d => d.backtestValue}
-						fill={(d) => d.backtestValue > 0 ? "#6BA583" : "#FF0000"}
-						baseAt={(xScale, yScale, d) => yScale(0)} />
-				</Chart>
-				<Chart id={4} yExtents={(d: DataRow) => d.backtestResult} height={400} origin={(w, h) => [0, h - 400]}>
-					<XAxis axisAt="bottom" orient="bottom" zoomEnabled={zoomEvent} {...xGrid} />
-					<YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".2s")} zoomEnabled={zoomEvent} />
-
-					<MouseCoordinateX at="bottom" orient="bottom" displayFormat={timeFormat("%Y-%m-%d %H:%M:%S")} />
-					<MouseCoordinateY at="left" orient="left" displayFormat={format(".4s")} />
-
-					<LineSeries
-				    yAccessor={d => d.backtestResult} />
-				</Chart>
+				{this.getCharts().map(({height, getComponent}) => getComponent({height}))}
 				<CrossHairCursor />
 			</ChartCanvas>
 		);
+	}
+
+	getCharts() {
+
+		const { width } = this.props;
+		const { zoomEvent } = this.props;
+
+		const margin = { left: 70, right: 70, top: 20, bottom: 30 };
+
+		const height = 1120;
+
+		const gridHeight = height - margin.top - margin.bottom;
+		const gridWidth = width - margin.left - margin.right;
+
+		const showGrid = true;
+		const yGrid = showGrid ? { innerTickSize: -1 * gridWidth, tickStrokeOpacity: 0.2 } : {};
+		const xGrid = showGrid ? { innerTickSize: -1 * gridHeight, tickStrokeOpacity: 0.2 } : {};
+
+		return [{
+			height: 400,
+			getComponent: ({height}) => <Chart id={1}
+				yExtents={d => [d.high, d.low]}
+				height={height}>
+				<YAxis axisAt="right" orient="right" ticks={5} zoomEnabled={zoomEvent} {...yGrid} />
+
+				<MouseCoordinateY
+					at="right"
+					orient="right"
+					displayFormat={format(".2f")} />
+
+				<CandlestickSeries />
+				<OHLCTooltip origin={[-40, 0]}/>
+				<ZoomButtons
+					onReset={this.handleReset}
+				/>
+			</Chart>
+		},
+		{
+			height: 150,
+			getComponent: ({height}) => <Chart id={2} yExtents={d => d.volume} height={height} origin={(w, h) => [0, h - 700]}>
+				<YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".2s")} zoomEnabled={zoomEvent} />
+
+				<MouseCoordinateY at="left" orient="left" displayFormat={format(".4s")} />
+
+				<BarSeries yAccessor={d => d.volume} fill={(d) => d.close > d.open ? "#6BA583" : "#FF0000"} />
+			</Chart>
+		},
+		{
+			height: 150,
+			getComponent: ({height}) => <Chart id={3} yExtents={(d: DataRow) => Math.sign(Math.random() - 0.5) * 200} height={height} origin={(w, h) => [0, h - 550]}>
+				<YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".2s")} zoomEnabled={zoomEvent} />
+
+				<MouseCoordinateX at="bottom" orient="bottom" displayFormat={timeFormat("%Y-%m-%d %H:%M:%S")} />
+				<MouseCoordinateY at="left" orient="left" displayFormat={format(".4s")} />
+
+				<BarSeries
+					yAccessor={d => d.backtestValue}
+					fill={(d) => d.backtestValue > 0 ? "#6BA583" : "#FF0000"}
+					baseAt={(xScale, yScale, d) => yScale(0)} />
+			</Chart>
+		},
+		{
+			height: 400,
+			getComponent : ({height}) => <Chart id={4} yExtents={(d: DataRow) => d.backtestResult} height={height} origin={(w, h) => [0, h - 400]}>
+				<XAxis axisAt="bottom" orient="bottom" zoomEnabled={zoomEvent} {...xGrid} />
+				<YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".2s")} zoomEnabled={zoomEvent} />
+
+				<MouseCoordinateX at="bottom" orient="bottom" displayFormat={timeFormat("%Y-%m-%d %H:%M:%S")} />
+				<MouseCoordinateY at="left" orient="left" displayFormat={format(".4s")} />
+
+				<LineSeries
+					yAccessor={d => d.backtestResult} />
+			</Chart>
+		}]
 	}
 }
 
